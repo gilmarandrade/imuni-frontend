ef09a5a4d8d1eba04cb846a05acf8dbb8b3219d7
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-05-26 00:36:31 -0300

    teste express.js

--- app.js
+++ app.js
@@ -1,23 +1,75 @@
+const bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var MongoClient = require('mongodb').MongoClient;
-// var format = require('util').format;
+// var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://mongo_uniquati:27017/uniquati', function(err, db) {
-    if(err)  {
-      throw err;
-    } else {
-      console.log('conectado ao banco uniquati');
-    }
+// MongoClient.connect('mongodb://mongo_uniquati:27017/uniquati', function(err, db) {
+//     if(err)  {
+//       throw err;
+//     } else {
+//       console.log('conectado ao banco uniquati');
+//     }
+// });
+
+
+// app.use(express.static(__dirname));
+
+// app.get('*', function(req, res){
+//   res.redirect('/');
+// });
+
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log('será que serei chamado antes?');
+  next();
+});
+
+app.get('/produtos/relatorio', (req, res, next) => {
+  res.json({ completo: req.query.completo, ano: req.query.ano });
+});
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.json({ id: req.params.id });
 });
 
 
-app.use(express.static(__dirname));
+app.post('/produtos/', (req, res, next) => {
+  res.send(req.body);
+});
 
-app.get('*', function(req, res){
-  res.redirect('/');
+
+app.get('/opa',(req, res, next) => {
+  console.log('durante');
+  res.json({
+    data: [
+      { 
+        name: "Ana Lúcia",
+        price: 122.5,
+        dicount: false,
+      },
+      { 
+        name: "Ana Lúcia 2",
+        price: 452.5,
+        dicount: null,
+      }
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200,
+  });
+  // res.send('estou bem');
+  next();
+});
+
+app.use('/opa', (req, res) => {
+  console.log('será que serei chamado depois?');
 });
 
 var port = 3000;
-app.listen(port);
-console.log('Uniquati-Umbler - consegui iniciar o servidor na porta %s', port);
+app.listen(port, ()=>{
+  console.log('Backend executando na porta %s', port);
+});
