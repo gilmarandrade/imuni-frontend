20184aa3bc7cb317303e5663e4fbae9f3400c119
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-05-27 18:47:03 -0300

    correção conexão mongo producao

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 module.exports = {
     authSecret: process.env.authSecret || 'asdsfghjkklçlkjhgf23456@#$%¨&*',
-    mongoUris: process.env.mongoUri || 'mongodb://localhost/knowledge_stats',
-    
+    mongoUris: process.env.mongoUris || 'mongodb://localhost/knowledge_stats',
+
     // acesso externo
     // mongodb://usuario:senha@kamino.mongodb.umbler.com:porta/banco
 
