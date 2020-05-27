2f26872e2babfea95d6822e87dede9144c5ce85e
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-05-27 17:05:41 -0300

    api stats MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const app = require('express')();
 const consign = require('consign');
+const mongoose = require('mongoose');
+
+require('./config/mongodb');
+
+app.mongoose = mongoose;
 
 consign()
   .include('./config/passport.js')
