1222ddb9c7ecdcdeb0c14b8ce9c5c72811fd5aab
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-05-27 17:26:23 -0300

    string de conexao producao

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
+// acesso externo:
+// mongodb://uniquati:2wt5p5cMoq65y4n@kamino.mongodb.umbler.com:49284/uniquati
+
+// acesso interno:
+// mongodb://uniquati:2wt5p5cMoq65y4n@mongo_uniquati:27017/uniquati
+
 // mongodb://mongo_uniquati:27017/uniquati'
-mongoose.connect('mongodb://mongo_uniquati:27017/uniquati', { useNewUrlParser: true, useUnifiedTopology: true })
+// mongodb://localhost/knowledge_stats
+mongoose.connect('mongodb://uniquati:2wt5p5cMoq65y4n@mongo_uniquati:27017/uniquati', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         const msg = "Não foi possível conectar com o MongoDB";
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
