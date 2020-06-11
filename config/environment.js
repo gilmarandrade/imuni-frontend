60dd6ea8d9c9b6e799dc9b008f4901a08816cbd2
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-06-11 09:52:20 -0300

    correção string conexão mongo

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 module.exports = {
     authSecret: process.env.authSecret || 'asdsfghjkklçlkjhgf23456@#$%¨&*',
-    mongoUris: process.env.mongoUris || 'mongodb://localhost:27017',
+    mongoUris: process.env.mongoUris || 'mongodb://localhost:27017/planilhas',
 
     // acesso externo
     // mongodb://usuario:senha@kamino.mongodb.umbler.com:porta/banco
