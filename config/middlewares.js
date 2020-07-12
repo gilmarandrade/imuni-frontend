02b3ca61479bdea908a1327f7c12e59c2b89033c
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-07-12 20:59:11 -0300

    cors policy servidor

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,12 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const corsOptions = {
+    origin: ['http://frenteprevencaocovidrn-org-br.umbler.net', 'http://localhost:8080'],
+    optionsSuccessStatus: 200
+}
+
 module.exports = app => {
     app.use(bodyParser.json());
-    app.use(cors());
+    app.use(cors(corsOptions));
 };
