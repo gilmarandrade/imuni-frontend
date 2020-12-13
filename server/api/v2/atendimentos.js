957160f89dff60c3de977a92be710307816c9cfe
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-12-13 12:21:05 -0300

    salvar atendimentos no banco

--- server/api/v2/atendimentos.js
+++ server/api/v2/atendimentos.js
@@ -9,8 +9,27 @@ module.exports = app => {
         try {
             atendimento.timestamp = (new Date(atendimento.timestamp)).toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' });
             console.log(atendimento);
-            await app.server.service.v2.atendimentoService.insertOne(atendimento);
-            return res.status(200).json(atendimento);
+
+            
+            if(atendimento && atendimento.responses) {
+                atendimento.items = {};
+
+                atendimento.responses.forEach(item => {
+                    atendimento.items[item.question.substring(1,4)] = {};
+                });
+
+                atendimento.responses.forEach(item => {
+                    atendimento.items[item.question.substring(1,4)][item.question.substring(4,7)] = item;
+                });
+
+                console.log(atendimento.items);
+                delete atendimento.responses;
+
+                await app.server.service.v2.atendimentoService.insertOne(atendimento);
+                return res.status(200).json(atendimento);
+            }
+
+
         } catch(err) {
             console.log(err);
             return res.status(500).send(err.toString());
