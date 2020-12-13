46aee14b135c5faf7398a280a8c566aff155f121
Gilmar Andrade <gilmar-andrade@outlook.com>
2020-12-12 21:46:27 -0300

    convert utc to local datetime

--- server/api/v2/atendimentos.js
+++ server/api/v2/atendimentos.js
@@ -7,7 +7,7 @@ module.exports = app => {
 
         console.log('ATENDIMENTO RECEBIDO:');
         try {
-            atendimento.timestamp = (new Date(atendimento.timestamp)).toLocaleDateString('pt-BR');
+            atendimento.timestamp = (new Date(atendimento.timestamp)).toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' });
             console.log(atendimento);
             await app.server.service.v2.atendimentoService.insertOne(atendimento);
             return res.status(200).json(atendimento);
