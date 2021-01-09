f7b1327e60a2ea461fa78ba35f45f9440f690d62
Gilmar Andrade <gilmar-andrade@outlook.com>
2021-01-08 21:37:13 -0300

    redirecionamento https

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -9,12 +9,12 @@ const corsOptions = {
 module.exports = app => {
 
     app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-        console.log('CHECANDO SSL')
+        // console.log('CHECANDO SSL')
         if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) { //Checa se o protocolo informado nos headers é HTTP 
-            console.log('é http')
+            // console.log('é http')
             res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
         } else {
-            console.log('ja é https')
+            // console.log('ja é https')
             //Se a requisição já é HTTPS 
             next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
         }
