219f0b2298064fae7af51aa295520ad7ff8c2817
Gilmar Andrade <gilmar-andrade@outlook.com>
2021-01-18 21:24:46 -0300

    formulario timestamp convertido de string para Date na inserção

--- server/api/v2/atendimentos.js
+++ server/api/v2/atendimentos.js
@@ -12,6 +12,7 @@ module.exports = app => {
         }
 
         try {
+            atendimento.timestamp = new Date(atendimento.timestamp);
             // atendimento.timestamp = (new Date(atendimento.timestamp)).toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' });
             // console.log(atendimento);
 
