82c18ee25f561c8077bb863fd6aaa91fd286eb59
Gilmar Andrade <gilmar-andrade@outlook.com>
2021-12-03 20:33:22 -0300

    static.js configuração heroku

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
       })
     ]
   },
-  outputDir: path.resolve(__dirname, "../server/public"),//TODO quando o servidro loca estiver rodando, não vai fazer build para a pasta dist...
+  outputDir: path.resolve(__dirname, "public"),//TODO quando o servidor local estiver rodando, não vai fazer build para a pasta dist...
 //   assetsDir: "../assets/",
     // Warning: Vue-CLI will delete the contents of whatever folders you specify to use for its output. To get around this, I created the "SPA" folders within my templates/ and static/ directories.
     // Note also that the assetsDir is specified relative to the outputDir.
